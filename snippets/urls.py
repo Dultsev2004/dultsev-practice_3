52f777490edcb02c1bedbfca00a409641d24a205
Sergey_Dultsev <121752321+Dultsev2004@users.noreply.github.com>
2023-11-23 21:50:59 +0700

    3.1

---
+++ snippets/urls.py
@@ -0,0 +1,13 @@
+from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+   path('', include(router.urls)),
+]
+
